e444db72bf3870d5b99155d4d3b7ed091569646b
Adiosiu <komangadi369@gmail.com>
2024-07-21 09:54:52 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -146,9 +146,9 @@ export default {
         this.fishList = JSON.parse(fishListData);
       } else {
         this.fishList = [
-          { nama: 'Ikan Nemo', habitat: 'Lautan tropis', pakan: 'Plankton', gambar: 'public/ikannemo.jpeg' },
-          { nama: 'Ikan Koi', habitat: 'Kolam taman', pakan: 'Pelet ikan', gambar: 'public/ikankoi.jpeg' },
-          { nama: 'Ikan Cupang', habitat: 'Air Tawar', pakan: 'Pelet, cacing', gambar: 'public/ikan cupang.jpeg' }
+          { nama: 'Ikan Nemo', habitat: 'Lautan tropis', pakan: 'Plankton', gambar: '/ikannemo.jpeg' },
+          { nama: 'Ikan Koi', habitat: 'Kolam taman', pakan: 'Pelet ikan', gambar: '/ikankoi.jpeg' },
+          { nama: 'Ikan Cupang', habitat: 'Air Tawar', pakan: 'Pelet, cacing', gambar: '/ikan cupang.jpeg' }
         ];
         this.saveFishList();
       }
